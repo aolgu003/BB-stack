8e56a5154f7afe300919bee6562b0861d9f1bbce
aolgu003 <andrew.olguin556@gmail.com>
2017-08-19 13:47:18 -0400

    Moved from docker to vagrant for dev environment and added scripts for provisioning

--- Dockerfile
+++
@@ -1,2 +0,0 @@
-FROM approximator/qtcreator
-
--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,13 @@
-docker pull approximator/qtcreator
+#!/usr/bin/env bash
 
-docker build -t approximator/qtcreator .  
+apt-get update
+apt-get install -y cmake
+apt-get install -y build-essential
+apt-get install -y lcov
+add-apt-repository ppa:levi-armstrong/qt-libraries-xenial
+add-apt-repository ppa:levi-armstrong/ppa
+apt-get update 
+apt-get install -y qt57creator-plugin-ros
+add-apt-repository --remove ppa:beineri/opt-qt57-xenial
+add-apt-repository --remove ppa:beineri/opt-qt571-xenial
+ln -s /opt/qt57/bin/qtcreator /usr/local/bin/
