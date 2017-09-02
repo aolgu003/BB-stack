32e7d48bef84b8f58a715bcc25518e83eb4edf8b
aolgu003 <andrew.olguin556@gmail.com>
2017-09-02 15:38:14 -0400

    Added zeromq to environment and cross compiled/ran hello world zeromq code using qemu

--- setup_env.sh
+++ setup_env.sh
@@ -3,7 +3,10 @@
 apt-get update
 apt-get install -y cmake
 apt-get install -y build-essential
+apt-get install -y libglib2.0-dev
 apt-get install -y lcov
+
+
 add-apt-repository ppa:levi-armstrong/qt-libraries-xenial
 add-apt-repository ppa:levi-armstrong/ppa
 apt-get update 
@@ -17,3 +20,21 @@ apt-get install -y g++-arm-linux-gnueabihf
 apt-get install -y libc6-dev-armhf-cross
 apt-get install -y qemu-user-static
 apt install -y qemu-user
+
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$LCM_INSTALL_DIR/pkgconfig
+
+wget https://github.com/zeromq/libzmq/releases/download/v4.2.2/zeromq-4.2.2.tar.gz
+tar -xf zeromq-4.2.2.tar.gz
+cd zeromq-4.2.2/
+./configure --host=arm-linux-gnueabihf
+make
+make install
+
+wget https://github.com/zeromq/cppzmq/archive/v4.2.2.tar.gz
+tar -xf v4.2.2.tar.gz
+cd cppzmq-4.2.2/
+mkdir build
+cd build/
+cmake .. -DCMAKE_CXX_COMPILER="/usr/bin/arm-linux-gnueabihf-g++" -DCMAKE_C_COMPILER="/usr/bin/arm-linux-gnueabihf-gcc"
+make
+make install
---
+++ src/zmqTests/hellozmq-server/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unistd.h>
+#include "zmq.hpp"
+#include <string>
+
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    zmq::context_t context (1);
+    zmq::socket_t socket (context, ZMQ_REP);
+    socket.bind ("tcp://*:5555");
+
+    while (true) {
+        zmq::message_t request;
+
+        //  Wait for next request from client
+        socket.recv (&request);
+        std::cout << "Received Hello" << std::endl;
+
+        //  Do some 'work'
+        sleep(1);
+
+        //  Send reply back to client
+        zmq::message_t reply (5);
+        memcpy (reply.data (), "World", 5);
+        socket.send (reply);
+    }
+    return 0;
+    return 0;
+}
